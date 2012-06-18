75e4409d4999b35b4aaf5ff511b0655da961b2aa
Paul deGrandis <paul.degrandis@gmail.com>
2012-06-18 15:30:19 -0400

    project update

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shoreleave-remote-noir "0.2.1"
+(defproject shoreleave/shoreleave-remote-noir "0.2.1"
   :description "A smarter client-side with ClojureScript : Shoreleave's noir hookups for secure http-rpc"
   :url "http://github.com/shoreleave"
   :license {:name "Eclipse Public License - v 1.0"
@@ -7,10 +7,8 @@
             :comments "See the notice in README.mkd or details in LICENSE_epl.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.3.0-beta8"]]
-  :dev-dependencies [[vimclojure/server "2.3.3" :exclusions [org.clojure/clojure]] 
-                     ;[cdt "1.2.6.2-SNAPSHOT"]
+  :dev-dependencies [;[cdt "1.2.6.2-SNAPSHOT"]
                      ;[lein-cdt "1.0.0"] ; use lein cdt to attach
                      ;[lein-autodoc "0.9.0"]
-                     [lein-marginalia "0.7.1"]]
-  :plugins  [[lein-cljsbuild "0.2.1"]])
+                     [lein-marginalia "0.7.1"]])
 
